5eb9f5c5fb216547824a82eae339ed6e3e508087
Zavier Divelbiss <semiviral@gmail.com>
2019-10-27 13:02:25 -0700

    code cleanup

--- NodeNetwork/Sigmoid.cs
+++ NodeNetwork/Sigmoid.cs
@@ -1,20 +1,18 @@
-﻿#region usings
+﻿#region
 
 using System;
 
 #endregion
 
-namespace NodeNetwork {
+namespace NodeNetwork
+{
     // todo: Implement sigmoid function as weight algorithm
     // might not do, I actually haven't gotten around to
     // understanding what a sigmoid function does.
-    public static class Sigmoid {
-        public static double Output(double x) {
-            return x < -45.0 ? 0.0 : x > 45.0 ? 1.0 : 1.0 / (1.0 + Math.Exp(-x));
-        }
+    public static class Sigmoid
+    {
+        public static double Output(double x) => x < -45.0 ? 0.0 : x > 45.0 ? 1.0 : 1.0 / (1.0 + Math.Exp(-x));
 
-        public static double Derivative(double x) {
-            return x * (1 - x);
-        }
+        public static double Derivative(double x) => x * (1 - x);
     }
 }
